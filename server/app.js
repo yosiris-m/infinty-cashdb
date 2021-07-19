683b20b645d76ff0047f827b375d84044927a9ed
Yosiris Mariñez <yosiris0510@gmail.com>
2021-07-19 21:44:08 +0200

    use 8080 port by default

--- server/app.js
+++ server/app.js
@@ -5,7 +5,7 @@ const transactions = require("./routes/transaction");
 const categories = require("./routes/category");
 const db = require("./db");
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 const app = express();
 
 db.connect();
